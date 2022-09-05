6161510e1f026644dcb3fb08cdb86ce3455cecf1
Kevin Liu <kevinliusingapore@gmail.com>
2022-09-06 03:46:37 +0800

    feature: added initial structure for B+ tree

---
+++ lib/bptree.cpp
@@ -0,0 +1,5 @@
+#include "node.h";
+
+class BPTree {
+    Node *root;
+};
---
+++ lib/node.cpp
@@ -0,0 +1,13 @@
+#include <vector>
+#include "bptree.h"
+
+using namespace std;
+
+int MAX_SIZE = 3;
+
+
+class Node {
+    friend class BPTree;
+};
+
+Node::Node() {};
